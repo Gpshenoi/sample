2307204f6dd280dbea8f2ecfe820e2b7067d2923
Gpshenoi <shenoi1993@gmail.com>
2018-11-20 23:09:24 +0530

    added new

---
+++ src/sample/sample.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class sample {
+		public void collegeName()
+		{System.out.println("collegename is payachayappas");
+		
+		}
+		public void collegeCode()
+		{System.out.println("college code is 12345");
+		}
+		
+		public void collegeRank ()
+		
+		{System.out.println("college rank is 1st always");
+		
+		}
+		
+		
+		
+		
+	}
+
+
